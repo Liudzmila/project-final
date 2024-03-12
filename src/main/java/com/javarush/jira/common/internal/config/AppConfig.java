f403ec08edcaabe3a5e61ab9a1c2e499dad0405a
Liudzmila Sobaleva <lusya88@gmail.com>
2024-03-13 00:41:18 +0530

    Done Task4 (added 2 beans in AppConfig.java, didn't use application-test.yaml)

--- src/main/java/com/javarush/jira/common/internal/config/AppConfig.java
+++ src/main/java/com/javarush/jira/common/internal/config/AppConfig.java
@@ -5,18 +5,21 @@ import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.hibernate5.jakarta.Hibernate5JakartaModule;
 import com.javarush.jira.common.util.JsonUtil;
+import liquibase.integration.spring.SpringLiquibase;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.*;
 import org.springframework.core.env.Environment;
 import org.springframework.core.env.Profiles;
 import org.springframework.http.ProblemDetail;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
+import javax.sql.DataSource;
 import java.util.Map;
 import java.util.concurrent.Executor;
 
@@ -28,10 +31,55 @@ import static com.fasterxml.jackson.annotation.JsonAutoDetect.Visibility.NONE;
 @EnableCaching
 @RequiredArgsConstructor
 @EnableScheduling
+@PropertySource("classpath:app-config.properties")
 public class AppConfig {
 
     private final AppProperties appProperties;
     private final Environment env;
+
+    @Value("${POSTGRES_URL}")
+    private String postgresUrl;
+
+    @Value("${DB_USERNAME}")
+    private String postgresUsername;
+
+    @Value("${DB_PASSWORD}")
+    private String postgresPassword;
+
+    @Value("${POSTGRES_DRIVER_CLASSNAME}")
+    private String postgresDriverClassName;
+
+    @Value("${H2_URL}")
+    private String h2Url;
+
+    @Value("${H2_USERNAME}")
+    private String h2Username;
+
+    @Value("${H2_PASSWORD}")
+    private String h2Password;
+
+    @Value("${H2_DRIVER_CLASSNAME}")
+    private String h2DriverClassName;
+
+    @Value("${CHANGELOG}")
+    private String changelog;
+
+    @Value("${CHANGELOG_TEST}")
+    private String changelogTest;
+
+    @Bean
+    public SpringLiquibase liquibase(DataSource dataSource) {
+        SpringLiquibase liquibase = new SpringLiquibase();
+        liquibase.setDataSource(dataSource);
+
+        if (isProd()) {
+            liquibase.setChangeLog(changelog);
+        }
+        if (isTest()) {
+            liquibase.setChangeLog(changelogTest);
+        }
+        return liquibase;
+    }
 
     @Bean("mailExecutor")
     Executor getAsyncExecutor() {
@@ -65,5 +113,27 @@ public class AppConfig {
     interface MixIn {
         @JsonAnyGetter
         Map<String, Object> getProperties();
+    }
+
+    @Profile("prod")
+    @Bean
+    public DataSource postgresDataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setUrl(postgresUrl);
+        dataSource.setUsername(postgresUsername);
+        dataSource.setPassword(postgresPassword);
+        dataSource.setDriverClassName(postgresDriverClassName);
+        return dataSource;
+    }
+
+    @Profile("test")
+    @Bean
+    public DataSource h2DataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setUrl(h2Url);
+        dataSource.setUsername(h2Username);
+        dataSource.setPassword(h2Password);
+        dataSource.setDriverClassName(h2DriverClassName);
+        return dataSource;
     }
 }
